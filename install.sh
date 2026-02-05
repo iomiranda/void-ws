1aeb53bc1105eb094b6388c6e4460b4cc7c2344d
iomiranda <miranda.ianpaul@gmail.com>
2026-02-04 17:40:50 -0800

    Create install shell script

---
+++ install.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# Script to install the usual packages in my personal workstations.
+# OS: Void Linux
+# Prerequisites: run void-installer, update xbps, install git, git clone main repo
+# *NOTE: Install the necessary firmware depending on device hardware
+# Credit to tony-btw. His YouTube channel helped with learning and utilizing Void Linux.
+#-----------------------------------------------------------------
+#                    BASE INSTALLATIONS & CONFIGS
+#-----------------------------------------------------------------
+# Install packages to get basic functions working
+echo 'Starting base installations and configs...'
+
+# Packages to be installed:
+sudo xbps-install -y basedevel vim grub efibootmgr
+
+# Install display server, allow it to start
+sudo xbps-install -Sy xorg xinit
+
+# Install wm and basic applications
+sudo xbps-install -Sy i3 i3status alacritty firefox dmenu neovim
+
+# Configure xinitrc
+# Add execute i3wm with startx
+echo 'exec i3' >> .xinitrc
+
+echo 'Base installations and configs finished...'
+#-----------------------------------------------------------------
+#               	    +ADDITIONAL INSTALLS
+#-----------------------------------------------------------------
+# Install file manager
+sudo xbps-install -Sy fff
+
+# Install image viewer and background image setter
+sudo xbps-install -Sy feh
+
+# Install starship, make terminal prompts nicer
+sudo xbps-install -Sy starship
+
+# Install iwd, use iwctl
+# This allows for connection to 5Ghz wifi
+# Before running, remove wpa_supplicant service
+sudo xbps-install -Sy iwd
+
+# Install media player
+sudo xbps-install -Sy ffmpeg
+
+# Install pipewire
+# Figure this out...
+
+#-----------------------------------------------------------------
+#               	    +ADDITIONAL CONFIGS
+#-----------------------------------------------------------------
+# Copy from repo/configs directory, Paste to local destination
+
+
+
