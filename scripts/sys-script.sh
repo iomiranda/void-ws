7b4e1910ab8c24ed37d4dea37d4f151eb8972688
iomiranda <miranda.ianpaul@gmail.com>
2026-02-13 16:42:33 -0800

    Create scripts directory & Added system control bash script

---
+++ scripts/sys-script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+choice=$(echo -e "kill\nshutdown\nreboot\nzzz\nlogout" | dmenu -i -p "system ctrl > " -fn "JetBrainsMonoNL Nerd Font-12" -nb "#CCCCCC" -nf "#101010" -sb "#101010" -sf "#CCCCCC")
+
+case $choice in
+    kill) ps -u "$USER" -o pid,comm,%cpu,%mem | dmenu -i -l 10 -p kill: -fn "JetBrainsMonoNL Nerd Font-12" -nb "#CCCCCC" -nf "#101010" -sb "#101010" -sf "#CCCCCC" | awk '{print $1}' | xargs -r kill ;;
+    shutdown)sudo poweroff ;;
+    reboot)sudo reboot ;;
+    zzz)sudo zzz	;;
+    logout) i3-msg exit ;;
+    *) exit 1 ;;
+esac
+
