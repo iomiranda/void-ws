be4b3e41b6b3f14e0ef54f344143d6444d2ae313
iomiranda <miranda.ianpaul@gmail.com>
2026-02-13 16:49:29 -0800

    Added cancel to sys-script.sh

--- scripts/sys-script.sh
+++ scripts/sys-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-choice=$(echo -e "kill\nshutdown\nreboot\nzzz\nlogout" | dmenu -i -p "system ctrl > " -fn "JetBrainsMonoNL Nerd Font-12" -nb "#CCCCCC" -nf "#101010" -sb "#101010" -sf "#CCCCCC")
+choice=$(echo -e "kill\nshutdown\nreboot\nzzz\nlogout\ncancel" | dmenu -i -p "system ctrl > " -fn "JetBrainsMonoNL Nerd Font-12" -nb "#CCCCCC" -nf "#101010" -sb "#101010" -sf "#CCCCCC")
 
 case $choice in
     kill) ps -u "$USER" -o pid,comm,%cpu,%mem | dmenu -i -l 10 -p kill: -fn "JetBrainsMonoNL Nerd Font-12" -nb "#CCCCCC" -nf "#101010" -sb "#101010" -sf "#CCCCCC" | awk '{print $1}' | xargs -r kill ;;
@@ -8,6 +8,7 @@ case $choice in
     reboot)sudo reboot ;;
     zzz)sudo zzz	;;
     logout) i3-msg exit ;;
+    cancel) pkill dmenu ;;
     *) exit 1 ;;
 esac
 
